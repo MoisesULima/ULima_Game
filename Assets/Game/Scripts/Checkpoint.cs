0f02d9ffb5ce3443a66804e87167bb0cd990f9b4
MoisesULima <jcumpa@ulima.edu.pe>
2025-11-21 16:28:07 -0500

    Add Animation for Checkpoint

---
+++ Assets/Game/Scripts/Checkpoint.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            animator.SetTrigger("Activate");
+        }
+    }
+}
---
+++ Assets/Game/Scripts/CheckpointManager.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private List<Checkpoint> checkpointList;
+    
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
