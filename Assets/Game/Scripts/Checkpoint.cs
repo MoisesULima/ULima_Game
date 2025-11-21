55a9f5fc2797669f34ad6ad3e679e3cd51b9f3c6
MoisesULima <jcumpa@ulima.edu.pe>
2025-11-21 16:45:24 -0500

    Update Checkpoint Funcionality, only one checkpoint is active

--- Assets/Game/Scripts/Checkpoint.cs
+++ Assets/Game/Scripts/Checkpoint.cs
@@ -3,17 +3,38 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour
 {
     private Animator animator;
+    private CheckpointManager manager;
+    private int id;
+    private bool isActive;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        isActive = false;
+    }
+
+    public void Init(CheckpointManager manager, int id)
+    {
+        this.manager = manager;
+        this.id = id;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && !isActive)
         {
             animator.SetTrigger("Activate");
+            isActive = true;
+            manager.ChangeActivation(id);
+        }
+    }
+
+    public void Deactivate()
+    {
+        if (isActive)
+        {
+            animator.SetTrigger("Deactivate");
+            isActive = false;
         }
     }
 }
--- Assets/Game/Scripts/CheckpointManager.cs
+++ Assets/Game/Scripts/CheckpointManager.cs
@@ -5,10 +5,24 @@ public class CheckpointManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private List<Checkpoint> checkpointList;
-    
+
     private void Start()
     {
-        
+        for(int i = 0; i < checkpointList.Count; i++)
+        {
+            checkpointList[i].Init(this, i);
+        }
+    }
+
+    public void ChangeActivation(int idActivated)
+    {
+        for(int i = 0; i < checkpointList.Count; i++)
+        {
+            if (i != idActivated)
+            {
+                checkpointList[i].Deactivate();
+            }
+        }
     }
 
     // Update is called once per frame
